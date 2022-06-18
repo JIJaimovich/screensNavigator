3737f42df11f399db9beb6c69bfb22093179e93d
Juan Ignacio <jaimovich.ignacio@gmail.com>
2022-06-18 14:42:46 -0300

    fonts

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import { ActivityIndicator } from 'react-native';
 
 export default function App() {
+  const [loaded] = useFonts({
+    OpenSans: require('./assets/fonts/OpenSans-Regular.ttf'),
+    Italic: require('./assets/fonts/OpenSans-Italic.ttf'),
+  });
+  
+  if (!loaded) {
+    return <ActivityIndicator />;
+  } 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text1}>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text2}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +28,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text1: {
+    fontFamily: 'OpenSans',
+    fontSize: 30,
+  },
+  text2: {
+    fontFamily: 'Italic',
+    fontSize: 15,
+  }
 });
